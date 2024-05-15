82b06916fa1ca489e1f4f4b563b4ae58167c494b
nicolmarin <nicolmoreno090905@gmail.com>
2024-05-15 12:14:51 -0500

    arreglos

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -1,5 +1,7 @@
 package co.edu.uniquindio.poo;
 
+import javax.sound.sampled.SourceDataLine;
+
 /**
  * Hello world!
  *
@@ -7,5 +9,14 @@ package co.edu.uniquindio.poo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    
+    Moto motoClasica = new Moto("Honda", "CB750", "javier", TipoMoto.CLASICA);
+    System.out.println("Moto: " + motoClasica.getPlaca() + " " + motoClasica.getModelo() + " " + motoClasica.getPropietario() + " " + motoClasica.getTipo());
+
+    // Crear un carro
+    Carro carro = new Carro("Toyota", "Corolla", "daniel");
+    System.out.println("Carro: " + carro.getPlaca() + " " + carro.getModelo() + " " + carro.getPropietario());
     }
 }
+
+
