555a836e71450b9765be41d76e37f2151fc700dc
nicolmarin <nicolmoreno090905@gmail.com>
2024-05-15 11:10:36 -0500

    Update CarroTest.java

---
+++ src/test/java/co/edu/uniquindio/poo/CarroTest.java
@@ -0,0 +1,36 @@
+package co.edu.uniquindio.poo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CarroTest {
+    private static final Logger LOG = Logger.getLogger(AppTest.class.getName());
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        LOG.info("Iniciado test shouldAnswerWithTrue");
+        assertTrue(true);
+        LOG.info("Finalizando test shouldAnswerWithTrue");
+    }
+    @Test
+    public void PlacaRepetida2() {
+        LOG.info("Iniciando test PlacaRepetida2");
+
+        Vehiculo vehiculo = new Vehiculo("carro");
+        Carro carro1 = new Carro("TAB13G","AUDI XX","DAVID FERNANDEZ HOGUERA");
+        Carro carro2 = new Carro("TAB13G","SUZUKI FOX","VALERY ANDREA MARTINEZ");
+
+        vehiculo.carroAdd(carro1);
+        assertThrows(Throwable.class, () -> vehiculo.carroAdd(carro2));
+
+        LOG.info("Finalizando test PlacaRepetida2");
+    }
+
+}
